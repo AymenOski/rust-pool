397e11dc469891235cb6254989a435f5cceef8d4
AymenOski <aymenazizi610@gmail.com>
2026-01-04 20:42:04 +0100

    basic operations with rust

--- scalar/target/debug/libscalar.d
+++
@@ -1,1 +0,0 @@
-/home/ayazizi/Desktop/piscine-rust/scalar/target/debug/libscalar.rlib: /home/ayazizi/Desktop/piscine-rust/scalar/src/lib.rs
--- scalar/target/debug/scalar.d
+++
@@ -1,1 +0,0 @@
-/home/ayazizi/Desktop/piscine-rust/scalar/target/debug/scalar: /home/ayazizi/Desktop/piscine-rust/scalar/src/lib.rs /home/ayazizi/Desktop/piscine-rust/scalar/src/main.rs
