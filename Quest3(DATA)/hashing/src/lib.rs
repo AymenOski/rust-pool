d004a10f6b9f3809cc27c9655f1a7642f4c008f7
AymenOski <aymenazizi610@gmail.com>
2026-01-08 23:42:52 +0100

    hashing

--- Quest3(DATA)/hashing/src/lib.rs
+++ Quest3(DATA)/hashing/src/lib.rs
@@ -1,9 +1,46 @@
-use hashing::*;
+pub fn mean(list: &[i32]) -> f64 {
+    if list.is_empty() {
+        return 0.0;
+    }
+    let sum: i32 = list.iter().sum();
+    sum as f64 / list.len() as f64
+}
 
-fn main() {
-    let v = [4, 7, 5, 2, 5, 1, 3];
+pub fn median(list: &[i32]) -> i32 {
+    let mut sorted = list.to_vec();
+    sorted.sort();
 
-    println!("mean {}", mean(&v));
-    println!("median {}", median(&v));
-    println!("mode {}", mode(&v));
+    let mid = sorted.len() / 2;
+
+    if sorted.len() % 2 == 0 {
+        let a = sorted[mid - 1];
+        let b = sorted[mid];
+        (a + b) / 2
+    } else {
+        sorted[mid]
+    }
+}
+
+pub fn mode(list: &[i32]) -> i32 {
+    if list.is_empty() {
+        return 0;
+    }
+
+    let mut freq = std::collections::HashMap::new();
+
+    for &num in list {
+        *freq.entry(num).or_insert(0) += 1;
+    }
+
+    let mut max_count = 0;
+    let mut mode_val = list[0];
+
+    for (&num, &count) in &freq {
+        if count > max_count {
+            max_count = count;
+            mode_val = num;
+        }
+    }
+
+    mode_val
 }
