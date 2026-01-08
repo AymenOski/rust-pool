702e8307430a45a1ac65cebd5a81fb6aef2ff75c
AymenOski <aymenazizi610@gmail.com>
2026-01-08 23:43:15 +0100

    bubble sort

---
+++ Quest3(DATA)/collect/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn bubble_sort(arr: &mut [i32]) {
+    if arr.is_empty() {
+        return;
+    }
+
+    let n = arr.len();
+    for i in 0..n {
+        for j in 0..n - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
