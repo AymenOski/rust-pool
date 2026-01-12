96f4577cfab4b0ba27cc6569e93c55f2cf9c613e
AymenOski <aymenazizi610@gmail.com>
2026-01-12 15:57:09 +0100

    Facilitates extensibility and error handling for a command-line flag system.

--- Quest4(ERRORS)/banner/src/lib.rs
+++ Quest4(ERRORS)/banner/src/lib.rs
@@ -1,12 +1,18 @@
 use std::{collections::HashMap, num::ParseFloatError};
 
 pub struct Flag {
-    // expected public fields
+    pub short_hand: String,
+    pub long_hand: String,
+    pub desc: String,
 }
 
-impl<'a> Flag<'a> {
+impl<'a> Flag {
     pub fn opt_flag(name: &'a str, d: &'a str) -> Self {
-        todo!()
+        Flag{
+            short_hand: format!("-{}", &name.chars().next().unwrap()),
+            long_hand: format!("--{}", name),
+            desc: d.to_string()
+        }
     }
 }
 
@@ -18,18 +24,31 @@ pub struct FlagsHandler {
 
 impl FlagsHandler {
     pub fn add_flag(&mut self, flag: Flag, func: Callback) {
-        todo!()
+        self.flags.insert(flag.short_hand, func);
+        self.flags.insert(flag.long_hand, func);
     }
 
-    pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
-        todo!()
+     pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
+        let func = self
+            .flags
+            .get(input)
+            .ok_or_else(|| "unknown flag".to_string())?;
+
+        let result = func(argv[0], argv[1]).map_err(|e| e.to_string())?;
+
+        Ok(result)
     }
 }
 
 pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
-    todo!()
+    let x: f32 = a.parse()?;
+    let y: f32 = b.parse()?;
+    let res = x / y;
+    Ok(res.to_string())
 }
-
 pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
-    todo!()
+    let x: f32 = a.parse()?;
+    let y: f32 = b.parse()?;
+    let res = x % y;
+    Ok(res.to_string())
 }
