c4009059cc7caf4ddc21f2e60e1e4cca2f7169b6
AymenOski <aymenazizi610@gmail.com>
2026-01-27 16:03:39 +0100

    working with json, hashmap and DateTime

--- Quest8(TRAITS)/commits_stats/src/main.rs
+++ Quest8(TRAITS)/commits_stats/src/main.rs
@@ -1,46 +1,13 @@
-use commits_stats::*;
-use std::collections::HashMap;
+use commits_stats::{commits_per_week, commits_per_author};
 
-#[inline]
-fn data() -> json::JsonValue {
-    json::parse(include_str!("../commits.json")).unwrap()
+fn main() {
+	let contents = include_str!("../commits.json");
+	let serialized = json::parse(&contents).unwrap();
+	println!("{:?}", commits_per_week(&serialized));
+	println!("{:?}", commits_per_author(&serialized));
 }
 
-#[test]
-fn test_commits_per_week() {
-    assert_eq!(
-        commits_per_week(&data()),
-        HashMap::from([
-            ("2020-W47".to_string(), 3),
-            ("2020-W43".to_string(), 1),
-            ("2020-W36".to_string(), 1),
-            ("2020-W50".to_string(), 2),
-            ("2020-W40".to_string(), 2),
-            ("2020-W44".to_string(), 5),
-            ("2020-W46".to_string(), 4),
-            ("2020-W31".to_string(), 1),
-            ("2020-W45".to_string(), 4),
-            ("2020-W49".to_string(), 7),
-        ])
-    );
-}
-
-#[test]
-fn test_commits_per_author() {
-    assert_eq!(
-        commits_per_author(&data()),
-        HashMap::from([
-            ("RPigott".to_string(), 1),
-            ("RedSoxFan".to_string(), 1),
-            ("Xyene".to_string(), 7),
-            ("paul-ri".to_string(), 2),
-            ("JayceFayne".to_string(), 1),
-            ("mwenzkowski".to_string(), 3),
-            ("psnszsn".to_string(), 1),
-            ("emersion".to_string(), 10),
-            ("tamirzb".to_string(), 1),
-            ("ifreund".to_string(), 1),
-            ("homembaixinho".to_string(), 2),
-        ])
-    )
-}
+// $ cargo run
+// {"2020-W44": 5, "2020-W36": 1, "2020-W31": 1, ... ,"2020-W45": 4, "2020-W46": 4}
+// {"homembaixinho": 2, "mwenzkowski": 3, ... ,"tamirzb": 1, "paul-ri": 2, "RPigott": 1}
+// $
