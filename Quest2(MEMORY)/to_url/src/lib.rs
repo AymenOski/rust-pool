4395b74b1b4f3755f39019b4731d787926283f6a
AymenOski <aymenazizi610@gmail.com>
2026-02-09 13:35:48 +0100

    'Hello, world!' to 'Hello,%20world!'

--- Quest2(MEMORY)/to_url/src/lib.rs
+++ Quest2(MEMORY)/to_url/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn to_url(s: &str) -> String {
     s.replace(" ", "%20")
 }
+
+// or :
+
+/*
+    pub fn to_url(s: &str) -> String {
+    let mut result = String::new();
+
+    for ch in s.chars() {
+        if ch == ' ' {
+            result.push_str("%20");
+        }else {
+            result.push(ch);
+        }
+    }
+    result
+}
+
+*/
