6ea8228cb771cf5ba216f9fe3371a781b61070be
AymenOski <aymenazizi610@gmail.com>
2026-02-09 11:29:28 +0100

    active learning and spaced repitition

--- Quest1(VARIABLES)/find_factorial/src/lib.rs
+++ Quest1(VARIABLES)/find_factorial/src/lib.rs
@@ -1,7 +1,18 @@
+//* recommendation (⚠️): use `Colorful Comments` extension for better readability of the comments in this file
+
 pub fn factorial(num: u64) -> u64 {
+    if num > 20 {
+        panic!("Input number is too large to compute factorial without overflow. 2^64 - 1 is the maximum value for u64, and 20! is the largest factorial that fits within this limit.");
+    }
+
     if num == 0 || num == 1 {
         return 1;
     }
 
     return factorial(num - 1) * num;
 }
+
+/*
+* Q1. What happens if the input number is greater than 20?
+    -A1. The factorial of numbers greater than 20 will exceed the maximum value that can be stored in a u64, leading to an overflow and incorrect results. In practice, you might want to handle this case to prevent such issues. 
+*/ 
