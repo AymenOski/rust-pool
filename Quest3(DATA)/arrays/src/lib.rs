166649eaaf9b017d8f908f15eea9b43541033218
AymenOski <aymenazizi610@gmail.com>
2026-02-11 17:09:21 +0100

    active learning and spaced repitition

--- Quest3(DATA)/arrays/src/lib.rs
+++ Quest3(DATA)/arrays/src/lib.rs
@@ -1,3 +1,5 @@
+//* recommendation (⚠️): use `Colorful Comments` extension for better readability of the comments in this file7
+
 pub fn sum(a: &[i32]) -> i32 {
     // let mut sum = 0;
     // for &val in a {
@@ -12,3 +14,7 @@ pub fn sum(a: &[i32]) -> i32 {
 pub fn thirtytwo_tens() -> [i32; 32] {
     [10; 32]
 }
+
+/*
+    &as we said before iter() returns an iterator that yields just references to the elements of the slice, so we can use it to iterate over the elements without consuming the slice itself.
+*/
--- Quest3(DATA)/arrays/src/main.rs
+++ Quest3(DATA)/arrays/src/main.rs
@@ -1,3 +1,5 @@
+//* recommendation (⚠️): use `Colorful Comments` extension for better readability of the comments in this file7
+
 use arrays::*;
 
 fn main() {
@@ -12,3 +14,13 @@ fn main() {
         thirtytwo_tens()
     );
 }
+
+/*
+    * Q & A:
+    * Q1. What does (1..=10).collect::<Vec<i32>>(); do?
+    -A1. It creates a range from 1 to 10 (inclusive) and collects it into a vector of i32.
+    its like we are creating an iterator that generates numbers from 1 to 10 and then using the magic wand which is collect() to turn it into a vector.
+    more accuratly ; it consumes the iterator and builds a vector from its yielded values.
+
+
+*/
