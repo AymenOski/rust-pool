fb265e09de07f50513ff0ffe92f7e7982aca58ef
AymenOski <aymenazizi610@gmail.com>
2026-02-17 16:53:17 +0000

    spaced repitition

--- Quest6(PATTERNS)/talking/src/lib.rs
+++ Quest6(PATTERNS)/talking/src/lib.rs
@@ -1,19 +1,19 @@
 pub fn talking(text: &str) -> &str {
-    if text.is_empty() {
+    if text.is_empty(){
         return "Just say something!";
     }
 
-    let has_letters = text.chars().any(|c| c.is_alphabetic());
-    let is_yelling = has_letters && text.chars().all(|c| !c.is_alphabetic() || c.is_uppercase());
-    let is_question = text.ends_with('?');
-
-    if is_yelling && is_question {
-        "Quiet, I am thinking!"
-    } else if is_yelling {
-        "There is no need to yell, calm down!"
-    } else if is_question {
-        "Sure."
-    } else {
-        "Interesting"
+    let is_question = text.ends_with("?");
+    let has_letter = text.chars().any(|c| c.is_alphabetic());
+    let is_yelling = text.chars().all(|c| !c.is_alphabetic() || c.is_uppercase());
+    
+    if is_question && is_yelling {
+        return "Quiet, I am thinking!";
+    }else if is_question {
+        return "Sure.";
+    }else if is_yelling && has_letter {
+        return "There is no need to yell, calm down!";
+    }else {
+        return "Interesting";
     }
 }
