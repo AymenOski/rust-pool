0790470c10c2e0153c19f3843a6197314bee5f99
AymenOski <aymenazizi610@gmail.com>
2026-02-17 15:19:33 +0000

    Exploiting external crates

--- Quest5(CRATES)/expected_variable/src/lib.rs
+++ Quest5(CRATES)/expected_variable/src/lib.rs
@@ -1,4 +1,6 @@
 use convert_case::{Case, Casing};
+use edit_distance::edit_distance;
+
 pub fn expected_variable(str1: &str, str2: &str) -> Option<String> {
     if str1.to_ascii_lowercase() == str1.to_ascii_lowercase().to_case(Case::Camel)
         || str1.to_ascii_lowercase() == str1.to_ascii_lowercase().to_case(Case::Snake)
@@ -19,37 +21,4 @@ pub fn expected_variable(str1: &str, str2: &str) -> Option<String> {
     }
 
     None
-}
-
-pub fn edit_distance(source: &str, target: &str) -> usize {
-    let a_chars: Vec<char> = source.chars().collect();
-    let b_chars: Vec<char> = target.chars().collect();
-
-    let len_a = a_chars.len();
-    let len_b = b_chars.len();
-
-    let mut dp = vec![vec![0; len_b + 1]; len_a + 1];
-
- 
-    for i in 0..=len_a {
-        dp[i][0] = i;
-    }
-    for j in 0..=len_b {
-        dp[0][j] = j;
-    }
-
-    for i in 1..=len_a {
-        for j in 1..=len_b {
-            if a_chars[i - 1] == b_chars[j - 1] {
-                dp[i][j] = dp[i - 1][j - 1]; 
-            } else {
-                dp[i][j] = 1 + std::cmp::min(dp[i - 1][j], std::cmp::min(dp[i][j - 1],dp[i - 1][j - 1], ));
-            }
-        }
-    }
-    for _row in &dp {
-        
-    }
-
-    dp[len_a][len_b]
 }
