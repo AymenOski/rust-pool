bae7e84d4e3549aaa1ce877b63fc3652ef4e6d0b
AymenOski <aymenazizi610@gmail.com>
2026-02-17 16:30:34 +0000

    how fun!

--- Quest6(PATTERNS)/pangram/src/lib.rs
+++ Quest6(PATTERNS)/pangram/src/lib.rs
@@ -1,5 +1,20 @@
-pub fn is_pangram(s: &str) -> bool {
-    let lower_s = s.to_lowercase();
+// pub fn is_pangram(s: &str) -> bool {
+//     let lower_s = s.to_lowercase();
 
-    ('a'..='z').all(|c| lower_s.contains(c))
+//     ('a'..='z').all(|c| lower_s.contains(c))
+// }
+
+use std::collections::HashMap;
+
+pub fn is_pangram(s: &str) -> bool {
+    let mut used:HashMap<char,bool> = HashMap::new();
+    
+
+    for ch in s.chars() {
+        if ch.is_ascii_alphabetic(){
+            // let _: () = x;
+            used.insert(ch.to_ascii_lowercase(), true);
+        }
+    }
+    used.len() >= 26
 }
