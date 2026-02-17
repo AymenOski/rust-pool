0874dac9dfc3ab5c75d5ff4cb3fe4fa0ef78f344
AymenOski <aymenazizi610@gmail.com>
2026-02-17 17:07:43 +0000

    yelling or not?

--- Quest6(PATTERNS)/talking/src/lib.rs
+++ Quest6(PATTERNS)/talking/src/lib.rs
@@ -1,19 +1,19 @@
 pub fn talking(text: &str) -> &str {
-    if text.is_empty(){
+    if text.is_empty() || text.chars().all(|c| c.is_whitespace()) {
         return "Just say something!";
     }
 
-    let is_question = text.ends_with("?");
-    let has_letter = text.chars().any(|c| c.is_alphabetic());
-    let is_yelling = text.chars().all(|c| !c.is_alphabetic() || c.is_uppercase());
-    
-    if is_question && is_yelling {
-        return "Quiet, I am thinking!";
-    } else if is_yelling && has_letter {
-        return "There is no need to yell, calm down!";
+    let has_letters = text.chars().any(|c| c.is_alphabetic());
+    let is_yelling = has_letters && text.chars().all(|c| !c.is_alphabetic() || c.is_uppercase());
+    let is_question = text.ends_with('?');
+
+    if is_yelling && is_question {
+        "Quiet, I am thinking!"
+    } else if is_yelling {
+        "There is no need to yell, calm down!"
     } else if is_question {
-        return "Sure.";
+        "Sure."
     } else {
-        return "Interesting";
+        "Interesting"
     }
 }
