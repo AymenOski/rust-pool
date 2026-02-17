2e082c5a962929302275839fadd33753aafe7fec
AymenOski <aymenazizi610@gmail.com>
2026-02-17 00:36:31 +0000

    active learning and spaced repitition

--- Quest4(ERRORS)/boxing_todo/src/lib.rs
+++ Quest4(ERRORS)/boxing_todo/src/lib.rs
@@ -53,3 +53,13 @@ impl TodoList {
     Ok(TodoList { title, tasks })
 }
 }
+
+/*
+    * Q & A:
+    * Q1 : What is Box<T> for?
+    - A1 : Box<T> is a smart pointer in Rust that allows you to store data on the heap instead of the stack. It provides ownership and automatic memory management for the data it points to.
+    how it works:
+    - When you create a Box<T>, it allocates memory on the heap for the data of type T and returns a pointer to that memory. The Box<T> itself is stored on the stack, but the data it points to is on the heap. This allows you to store large amounts of data without worrying about stack overflow, as the heap can grow dynamically. Additionally, Box<T> provides ownership semantics, meaning that when a Box<T> goes out of scope, the memory it points to is automatically deallocated, preventing memory leaks.
+    - In an ideal world, everything should be stored on the stack for performance reasons, but in practice, we often need to store data that is too large or has a size that cannot be determined at compile time. In such cases, we use Box<T> to store the data on the heap while still maintaining ownership and memory safety.
+    - also the compiler have to know the size of the type at compile time, but when we want to return a trait object (like Box<dyn Error>), we can't know the size of the concrete type that implements the trait at compile time. By using Box<dyn Error>, we can store any type that implements the Error trait on the heap and return a pointer to it.
+*/
