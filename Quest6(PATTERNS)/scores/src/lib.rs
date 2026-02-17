c2df5f9332291ce68a5ce2e9e2155d8119b3baba
AymenOski <aymenazizi610@gmail.com>
2026-02-17 15:52:27 +0000

    spaced repitition

--- Quest6(PATTERNS)/scores/src/lib.rs
+++ Quest6(PATTERNS)/scores/src/lib.rs
@@ -1,36 +1,46 @@
-use std::collections::HashMap;
+// pub fn score(word: &str) -> u64 {
+//     word.chars()
+//         .map(|x| match x.to_ascii_lowercase() {
+//             'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+//             'd' | 'g' => 2,
+//             'b' | 'c' | 'm' | 'p' => 3,
+//             'f' | 'h' | 'v' | 'w' | 'y' => 4,
+//             'k' => 5,
+//             'j' | 'x' => 8,
+//             'q' | 'z' => 10,
+//             _ => 0,
+//         })
+//         .sum()
+// }
 
-pub fn score(text: &str) -> u32 {
-    let mut scores = HashMap::new();
-    for c in "aeioulnrst".chars() {
-        scores.insert(c, 1);
+// &str "Hello"
+//    ↓
+// .chars()               → Iterator<Item = char>
+//    ↓
+// .map(|x| match ...)    → Iterator<Item = u64>
+//    ↓
+// .sum()                 → u64
+
+// or :
+pub fn score(s: &str) -> u64 {
+    let mut total = 0;
+
+    for c in s.chars() {
+        if c.is_ascii_alphabetic() {
+            let ch = c.to_ascii_uppercase();
+
+            total += match ch {
+                'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+                'D' | 'G' => 2,
+                'B' | 'C' | 'M' | 'P' => 3,
+                'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+                'K' => 5,
+                'J' | 'X' => 8,
+                'Q' | 'Z' => 10,
+                _ => 0,
+            };
+        }
     }
-    for c in "dg".chars() {
-        scores.insert(c, 2);
-    }
-    for c in "bcmp".chars() {
-        scores.insert(c, 3);
-    }
-    for c in "fhvwy".chars() {
-        scores.insert(c, 4);
-    }
-    scores.insert('k', 5);
-    for c in "jx".chars() {
-        scores.insert(c, 8);
-    }
-    for c in "qz".chars() {
-        scores.insert(c, 10);
-    }
-    text.to_lowercase()
-        .chars()
-        .filter_map(|c| scores.get(&c))
-        .sum::<u32>()
-    //or 
-    // let mut total = 0;
-    // for c in text.to_lowercase().chars() {
-    //     if let Some(value) = scores.get(&c) {
-    //         total += value;
-    //     }
-    // }
-    // return total;
+
+    total
 }
