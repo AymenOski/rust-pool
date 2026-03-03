f4557fd895ffdf4824bf2063216199bb6a91e775
AymenOski <aymenazizi610@gmail.com>
2026-03-03 16:09:21 +0000

    sales

---
+++ Quest10(FEATURES)/sales/src/lib.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Store {
+    pub products: Vec<(String, f32)>,
+}
+impl Store {
+    pub fn new(products: Vec<(String, f32)>) -> Store {
+        Store { products }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Cart {
+    pub items: Vec<(String, f32)>,
+    pub receipt: Vec<f32>,
+}
+impl Cart {
+    pub fn new() -> Cart {}
+    pub fn insert_item(&mut self, s: &Store, ele: String) {}
+    pub fn generate_receipt(&mut self) -> Vec<f32> {}
+}
